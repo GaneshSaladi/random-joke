2dfed232206459e6a55ca7a9205b54cea328262b
ganesh <ganeshsaladis77@gmail.com>
2023-05-11 14:14:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import axios from "axios";
+import { useState } from 'react';
 import './App.css';
-
 function App() {
+  const [joke, setJoke] = useState("")
+  const jokess = async ()=>{
+    const options = {
+      method: 'GET',
+      url: 'https://jokes-by-api-ninjas.p.rapidapi.com/v1/jokes',
+      headers: {
+        'X-RapidAPI-Key': 'f69a78740amsh259333f9a8e95e1p17f9cbjsn8d4fdbd71a43',
+        'X-RapidAPI-Host': 'jokes-by-api-ninjas.p.rapidapi.com'
+      }
+    };
+    try {
+      const response = await axios.request(options);
+      setJoke(response.data)
+      console.log(response.data[0].joke);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  const jokes=()=>{
+    jokess();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <span className='heading'>Jokes Generator</span>
+      {joke && <p>{joke[0].joke}</p>}
+      <button onClick={()=>{
+        jokes()
+      }} className='myButton'>Click me</button>
     </div>
   );
 }
